494fc04c6a0e3e5360299cc31f515e1ecdfe08db
deminearchiver <minearchiver@gmail.com>
2025-12-05 19:42:57 +0300

    Work in progress.

--- app_logic/lib/src/callibri_controller.dart
+++ app_logic/lib/src/callibri_controller.dart
@@ -1,1 +1,43 @@
-class CallibriController {}
+import 'package:neurosdk2/neurosdk2.dart';
+
+enum CallibriDataState { signal, envelope, mems }
+
+enum CallibriConnectionState {
+  connection,
+  connected,
+  disconnection,
+  disconnected,
+  error,
+}
+
+class CallibriInfo {
+  const CallibriInfo({
+    required this.name,
+    required this.adress,
+    required this.sensorInfo,
+  });
+
+  final String name;
+  final String adress;
+  final FSensorInfo sensorInfo;
+}
+
+class CallibriController {
+  CallibriController();
+
+  Scanner? _scanner;
+
+  Future<void> _createScanner() async {
+    if (_scanner != null) return;
+    final scanner = await Scanner.create(_filters);
+    _scanner = scanner;
+  }
+
+  void search({required Duration duration}) async {
+    final scanner = await Scanner.create(_filters);
+  }
+
+  void dispose() {}
+
+  static const _filters = <FSensorFamily>[.leCallibri, .leKolibri];
+}
